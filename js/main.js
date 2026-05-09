8f1772d40db8ce0398316425738d6329cad587cd
Zaara Mehta <thegivingcollective.tgc@gmail.com>
2026-05-09 16:11:21 -0400

    fix carousel autoplay on load

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const testimonialCarousel = document.querySelector('#testimonial-carousel');
     if (testimonialCarousel) {
-        const carousel = new bootstrap.Carousel(testimonialCarousel);
-
-        const carouselInner = testimonialCarousel.querySelector('.carousel-inner');
-        carouselInner.addEventListener('click', function() {
-            carousel.next();
+        const carousel = new bootstrap.Carousel(testimonialCarousel, {
+            interval: 3000
         });
     }
 });
