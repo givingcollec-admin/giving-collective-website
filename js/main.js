49765024a9663fd7091c8becc24e986e614e13cf
Zaara Mehta <thegivingcollective.tgc@gmail.com>
2026-05-09 15:34:17 -0400

    testimonial carousel controls

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-// This file is currently empty.
-// You can add JavaScript here to make your website interactive.
+document.addEventListener('DOMContentLoaded', function () {
+    const testimonialCarousel = document.querySelector('#testimonial-carousel');
+    if (testimonialCarousel) {
+        const carousel = new bootstrap.Carousel(testimonialCarousel);
+
+        const carouselInner = testimonialCarousel.querySelector('.carousel-inner');
+        carouselInner.addEventListener('click', function() {
+            carousel.next();
+        });
+    }
+});
